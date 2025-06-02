1f00ed15b1b68647d465cf666b2cbba8c66a3469
Kilian <dankestmirror@hotmail.es>
2025-06-02 14:27:22 +0100

    Se crea nuevo componente

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+<template>
+            <div v-for="guitarra in guitarras" :key="guitarra.id" class="col-md-6 col-lg-4 my-4 row align-items-center">
+                <div class="col-4">
+                    <img class="img-fluid" :src="`/img/${guitarra.imagen}.jpg`" alt="imagen guitarra">
+                </div>
+                <div class="col-8">
+                    <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+                    <p>{{ guitarra.descripcion }}</p>
+                    <p class="fw-black text-primary fs-3">$299</p>
+                    <button 
+                        type="button"
+                        class="btn btn-dark w-100 "
+                    >Agregar al Carrito</button>
+                </div>
+            </div><!-- FIN GUITARRA -->
+</template>
